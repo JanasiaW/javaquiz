7899210fe3fd2e36d01eed8b3e55377b6b8c2556
JanasiaW <59932787+JanasiaW@users.noreply.github.com>
2020-04-27 02:31:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,56 @@
 
-function check(){
+fucction check(){
 
-	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-	var question3 = document.quiz.question3.value;
-	var correct = 0;
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var question5 = document.quiz.question5.value;
+  var question6 = document.quiz.question6.value;
+  var question7 = document.quiz.question7.value;
+  var question8 = document.quiz.question8.value;
+  var question9 = document.quiz.question9.value;
+  var question10 = document.quiz.question10.value;
+  var correct = 0;
 
 
-	if (question1 == "Providence") {
-		correct++;
+  if (question1 == "If statements") {
+    correct++;
 }
-	if (question2 == "Hartford") {
-		correct++;
-}	
-	if (question3 == "Albany") {
-		correct++;
-	}
-	
-	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
-	var messages = ["Great job!", "That's just okay", "You really need to do better"];
-	var score;
+  if (question2 == "Operating systems") {
+    correct++;
+}
+  if (question3 == "Hash stores keys which creates representation of data") {
+    correct++;
+}
+  if (question4 == "Click button that says download") {
+    correct++;
+}
+  if (question5 == "new_hash=") {
+    correct++;
+}
+  if (question6 == "True") {
+    correct++;
+}
+  if (question7 == "True") {
+    correct++;
+}
+  if (question8 == "False") {
+    correct++;
+}
+  if (question9 == "False") {
+    correct++;
+}
+  if (question10 == "The.pop") {
+    correct++;
+}
 
-	if (correct == 0) {
-		score = 2;
-	}
+var picture = ["img/dog.jpg", "img/obama.jpg", "img/toy.jpg"];
+var message = ["Excellent job!", "You can do better next time.", "You got this! Try again."];
+var score;
 
-	if (correct > 0 && correct < 3) {
-		score = 1;
-	}
+document.getElementById("after_submit").style.visibility = "visible";
 
-	if (correct == 3) {
-		score = 0;
-	}
-
-	document.getElementById("after_submit").style.visibility = "visible";
-
-	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[score];
-	}
-	
+document.getElementById("message").innerHTML = message;
+document.getElementbyId("picture").src = pictures;
+}
